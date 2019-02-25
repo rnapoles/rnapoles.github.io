1c9cded4fc8889f3dc685d57fc100b3e19e8cfca
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2019-02-25 10:02:23 -0500

    - first commit

---
+++ resume/js/scripts.js
@@ -0,0 +1,53 @@
+var callback = function(){
+	
+    $('.item-skills').each(function(){
+		newWidth = $(this).parent().width() * $(this).data('percent');
+		$(this).width(0);
+        $(this).animate({
+            width: newWidth,
+        }, 1000);
+	});
+    
+	$('.icons-red').each(function(){
+		height = $(this).height();
+        $(this).animate({
+            height: 14,
+        }, 2000);
+	});
+    
+    jQuery(".fancybox").fancybox({
+    groupAttr: 'data-rel',
+    prevEffect: 'fade',
+    nextEffect: 'fade',
+    openEffect  : 'elastic',
+    closeEffect  : 'fade',
+    closeBtn: true,
+    helpers: {
+        title: {
+                type: 'float'
+            }
+        }
+    });
+
+    $(".fbox-modal").fancybox({
+        maxWidth    : 800,
+        maxHeight   : 600,
+        fitToView   : false,
+        width       : '70%',
+        height      : '70%',
+        autoSize    : false,
+        closeClick  : false,
+        closeEffect : 'fade',
+        openEffect  : 'elastic'
+    });    
+    
+};
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+	clearTimeout(resize);
+	resize = setTimeout(function(){
+		callback();
+	}, 100);
+};
