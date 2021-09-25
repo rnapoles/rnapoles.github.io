ed68ee542769792b5e5ea0ccf19b5e368293706f
Reinier Nápoles Martínez <rnapoles86@gmail.com>
2021-09-25 13:53:42 -0400

    * update-repo.sh refresh

--- update-repo.sh
+++ update-repo.sh
@@ -4,18 +4,25 @@ BASE_FOLDER=/root/www/mis-docs
 DST_FOLDER=/var/www/cv
 mkdir -p $DST_FOLDER
 
-dotlockfile -l -r 0 -p $DST_FOLDER/file.lock || exit 0                                                                             
+dotlockfile -l -r 0 -p $DST_FOLDER/file.lock                                                                           
+if [ $? -eq 0 ]; then
+  cd $BASE_FOLDER
+  
+  echo Descartando cambios
+  git stash
 
-cd $BASE_FOLDER
-git pull
+  echo Actualizando
+  git pull
+  chmod +x ./update-repo.sh
 
-cp -R $BASE_FOLDER/resume/* $DST_FOLDER/
-cp $BASE_FOLDER/conf/cv.conf /etc/apache2/sites-enabled/
+  cp -R $BASE_FOLDER/resume/* $DST_FOLDER/
+  cp $BASE_FOLDER/conf/cv.conf /etc/apache2/sites-enabled/
 
-service apache2 restart
+  service apache2 restart
 
-dotlockfile -u $DST_FOLDER/file.lock
+  dotlockfile -u $DST_FOLDER/file.lock
+  tailf /var/log/syslog
+fi
 
-tailf /var/log/syslog
 
 
